ea6ecbaa37b09c38f1de1f8ab2cb6eca02a56b34
RagtagTurtle <e.russell1792@gmail.com>
2018-06-20 21:30:05 -0500

    added backend and additional functionality

--- public/index.js
+++ public/index.js
@@ -5,48 +5,79 @@ var HomePage = {
   data: function() {
     return {
       tasks: [ 
-              {
-                id: 1,
-                text: "Mow the lawn",
-                completed: true  
-                },
-                {
-                id: 2,  
-                text: "Hack darts",
-                completed: true
-                },
-                {
-                id: 3,
-                text: "Win Parlays",
-                completed: true
-                }
-                ],
+        {
+          id: 1,
+          text: "Mow the lawn",
+          completed: false  
+        },
+        {
+          id: 2,  
+          text: "Hack darts",
+          completed: false
+        },
+        {
+          id: 3,
+          text: "Win Parlays",
+          completed: false
+        }
+      ],
       newTask: {
-                id: "",
-                text: "",
-                completed: ""
-                }
+        id: "",
+        text: "",
+        completed: ""
+      }
 
     };
   },
-  created: function() {},
+  created: function() {
+    axios
+      .get('api/tasks')
+      .then(function(response) {
+        this.tasks = response.data;
+      }.bind(this));
+  },
   methods: {
     addTask: function() {
       var newTaskInfo = {
-                        id: this.newTask.id,
-                        text: this.newTask.text,
-                        completed: this.newTask.completed
-                        };
-    if (this.newTask.text) {
-    this.tasks.push(newTaskInfo);
-    this.newTask.text = "";
-    }
-  },
+        id: this.newTask.id,
+        text: this.newTask.text,
+        completed: this.newTask.completed
+      };
+      if (this.newTask.text) {
+        this.tasks.push(newTaskInfo);
+        this.newTask.text = "";
+      }
+    },
+    toggleCompleted: function(inputTask) {
+      inputTask.completed = !inputTask.completed;
+    },
+    numberOfTasks: function() {
+      return this.tasks.length;
+    },
+    numberOfIncompleteTasks: function() {
+      var count = 0;
 
-  markComplete: function(inputTask) {
-    var indexOfTask = this.tasks.indexOf(inputTask);
-    this.tasks.splice(indexOfTask, 1);
-  }
+      this.tasks.forEach(function(task) {
+        if (!task.completed) { 
+          count++;
+        }
+      });
+      return count;
+    },
+    deleteCompleted: function() {
+      var incompleteTasks = [];
+      this.tasks.forEach(function(task) {
+        if (!task.completed) {
+          incompleteTasks.push(task);
+        }
+      });
+      this.tasks = incompleteTasks;
+    }
+
+  // markComplete: function(inputTask) {
+  //   var indexOfTask = this.tasks.indexOf(inputTask);
+  //   this.tasks.splice(indexOfTask, 1);
+  // }
 
   },
   computed: {}
